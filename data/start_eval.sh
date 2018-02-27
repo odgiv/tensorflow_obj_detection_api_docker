55fb2ccd46fc18cf505dc3fa9feef6cbe4cf1e99
odgiiv <odgiiv.kh@gmail.com>
2018-02-27 11:38:18 +0100

    created sub folders in data dir and moved start scripts to data directory

--- data/start_eval.sh
+++ data/start_eval.sh
@@ -6,7 +6,7 @@ cd /home/object_detection_api/models/research/object_detection
 
 python eval.py \
 	--logtostderr \
-	--pipeline_config_path=/home/data/faster_rcnn_resnet101_numbers.config \
+	--pipeline_config_path=/home/data/configs/$1 \
 	--checkpoint_dir=/home/data/train_result/ \
 	--eval_dir=/home/data/eval_result/ &
 
