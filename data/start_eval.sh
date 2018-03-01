c3c37ae631c5f026b62942d31fc2a3269e95af8f
odgiiv <odgiiv.kh@gmail.com>
2018-03-01 09:06:46 +0100

    back to using earlier version of api models-master.zip

--- data/start_eval.sh
+++ data/start_eval.sh
@@ -5,13 +5,11 @@ set -x -e
 
 rm -rf /home/data/eval_result/*
 
-cd /home/object_detection_api/models/research/object_detection
+#cd /home/object_detection_api/models/research/object_detection
+cd /home/object_detection_api/models-master
 
 python eval.py \
 	--logtostderr \
 	--pipeline_config_path=/home/data/configs/$1 \
 	--checkpoint_dir=/home/data/train_result/ \
-	--eval_dir=/home/data/eval_result/ &
-
-cd ..
-
+	--eval_dir=/home/data/eval_result/
--- data/start_training.sh
+++ data/start_training.sh
@@ -5,13 +5,12 @@ set -x -e
 
 rm -rf /home/data/train_result/*
 
-cd /home/object_detection_api/models/research/object_detection
+#cd /home/object_detection_api/models/research/object_detection
+cd /home/object_detection_api/models-master
 
 python train.py \
 	--logtostderr \
 	--pipeline_config_path=/home/data/configs/$1 \
-	--train_dir=/home/data/train_result/ &
-
-cd ..
+	--train_dir=/home/data/train_result/ 
 
 
