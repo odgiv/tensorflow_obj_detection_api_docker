c45a79647cf27389ae2abfba92a0bda0c114bda8
odgiiv <odgiiv.kh@gmail.com>
2018-03-01 09:30:15 +0100

    .sh scripts fix

--- data/start_eval.sh
+++ data/start_eval.sh
@@ -6,9 +6,9 @@ set -x -e
 rm -rf /home/data/eval_result/*
 
 #cd /home/object_detection_api/models/research/object_detection
-cd /home/object_detection_api/models-master/object_detection
+cd /home/object_detection_api/models-master
 
-python eval.py \
+python object_detection/eval.py \
 	--logtostderr \
 	--pipeline_config_path=/home/data/configs/$1 \
 	--checkpoint_dir=/home/data/train_result/ \
