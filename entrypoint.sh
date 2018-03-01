308e8f0d33bac9a1002e98428f62387699d07cfa
odgiiv <odgiiv.kh@gmail.com>
2018-03-01 09:32:44 +0100

    .sh scripts fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+chmod +x /home/data/start_training.sh /home/data/start_eval.py /home/data/start_tensorboard.sh
+
 echo "export PYTHONPATH=$PYTHONPATH:/home/object_detection_api/models/research:/home/object_detection_api/models/research/slim" >> ~/.bashrc
 source ~/.bashrc
 
